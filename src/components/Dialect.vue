213a95a19f2ca8bc6e20f7f0c8fc51c493ceb5e6
yong <zengjunyong@gmail.com>
2016-10-17 17:05:45 +0800

    https://github.com/vuejs/vue-syntax-highlight/issues/3 webstorm support VUE file well but need to config it :-)

--- src/components/Dialect.vue
+++ src/components/Dialect.vue
@@ -4,10 +4,10 @@
     </div>
 </template>
 
-<script lang="coffee">
+<script lang="coffee" type="text/coffeescript">
     module.exports =
         data: ->
-            msg : 'I like writing CoffeeScript and SASS in vue file!'
+            msg: 'I like writing CoffeeScript and SASS in vue file!'
         methods:
             debug: ->
                 debugger
@@ -15,7 +15,7 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
+<style lang="scss" scoped rel="stylesheet/scss">
     $color: gray;
     .dialect {
         color: $color;
