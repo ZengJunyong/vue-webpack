ec48b4b29ff706855b30cf1d74ef3ccd8e752f60
yong <zengjunyong@gmail.com>
2016-10-17 15:53:36 +0800

    With Hot Reload, you can update the HTML and CSS, but your data is still there :-)

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,33 +1,39 @@
 <template>
-  <div class="hello">
-    <h1 @click="debug">{{ msg }}</h1>
-    <p>You stay at here <span class="count">{{count}}s</span></p>
-  </div>
+    <div class="hello">
+        <h1 @click="debug">{{ msg }}</h1>
+        <p>You stay at here <span class="count">{{count}}s</span></p>
+    </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Hello Vue 2!',
-      count: 0
+    export default {
+        data () {
+            return {
+                msg: 'Hello Vue 2!',
+                count: 0
+            }
+        },
+        methods: {
+            debug(){
+                debugger
+                console.log('debug');
+            }
+        },
+        mounted(){
+            console.log('mounted');
+            setInterval(()=> this.count++, 1000)
+        }
     }
-  },
-  methods:{
-    debug(){
-      debugger
-      console.log('debug');
-    }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  color: #42b983;
-}
-    .count{
-        color: red;
+    h1 {
+        color: #42b983;
+    }
+
+    .count {
+        color: yellow;
+        background: black;
     }
 </style>
