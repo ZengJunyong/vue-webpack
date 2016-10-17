30fc97000504bf658ccf814d42aa5a84c667b8bf
yong <zengjunyong@gmail.com>
2016-10-17 10:48:10 +0800

    vue init webpack webpack

---
+++ src/components/Hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1 @click="debug">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue 2! 屌'
+    }
+  },
+  methods:{
+    debug(){
+      debugger
+      console.log('debug');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
