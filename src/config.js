684d42a809f0e8f893ddef8ed1a6f80bf0dad981
Junyong <zengjunyong@gmail.com>
2016-11-28 11:31:58 +0800

    fleek stripe account

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   domain: 'https://letsgaigai.com/',
   // domain: 'http://localhost:4000/',
-  // key: 'pk_test_FX2nzQcClgXqETUTMZDK2BNu'
+  // key: 'pk_test_Im6kwYcvi4oVcY5afiu7E4YR'
   key: 'pk_live_Q0ADwm9KDqfKF6IofS13ivCg'
 }
