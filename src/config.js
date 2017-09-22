280952b2f86ae37c910c565c16b905a3f796d87b
yong <zengjunyong@gmail.com>
2017-09-22 11:09:37 +0800

    account is changed

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   domain: 'https://www.heydouble.com/',
   // domain: 'http://localhost:4000/',
   // key: 'pk_test_FX2nzQcClgXqETUTMZDK2BNu'
-  key: 'pk_live_Zo2921HDEtTxDEZfXS3ZVR5N'
+  key: 'pk_live_oOF1pHNJ106AEei12c1hsKE8'
 }
