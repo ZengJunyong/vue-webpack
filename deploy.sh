931ee2c74bff1f3b72a117a24ae332da080fca8f
Junyong <zengjunyong@gmail.com>
2017-10-11 11:26:55 +0800

    upgrade deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 npm run build
-cd ~/paktor/gokaikai/packages/system/public/assets/static/payment
-rm -rf *
-cp -rf ~/learn/vue.js/vue-cli/webpack/dist/* .
-cd ~/paktor/gokaikai
-git rm packages/system/public/assets/static/payment/*
-git add packages/system/public/assets/static/payment/*
+cd ~/paktor/gokaikai/packages/system/public/assets/static/payment && rm -rf *
+cd ~/paktor/gokaikai && git rm -r packages/system/public/assets/static/payment/*
+cd ~/paktor/gokaikai/packages/system/public/assets/static/payment && cp -rf ~/learn/vue.js/vue-cli/webpack/dist/* .
+cd ~/paktor/gokaikai && git add packages/system/public/assets/static/payment/*
 git commit -m "stripe update"
 git push
