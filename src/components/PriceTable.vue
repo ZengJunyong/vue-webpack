7ea5dc300baf6e66a70ff02ccf675428ee6f120a
Junyong <zengjunyong@gmail.com>
2018-04-02 16:13:29 +0800

    move the strip config from file to mongodb

--- src/components/PriceTable.vue
+++ src/components/PriceTable.vue
@@ -98,7 +98,7 @@
                                 next(vm => {
                                     let {query} = vm.$route
                                     const {membership, discount} = query;
-                                    vm.stripe = JSON.parse(res.body)[membership][discount]
+                                    vm.stripe = res.body[membership][discount]
                                     vm.query = query
                                 })
                             }
