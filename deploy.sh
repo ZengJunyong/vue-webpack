f4a6658a459ed55c028180a09bef8533055824aa
yong <zengjunyong@gmail.com>
2018-06-22 12:13:54 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-npm run build
-cd ~/paktor/web-gaigai/packages/system/public/assets/static/payment && rm -rf *
-cd ~/paktor/web-gaigai && git rm -r packages/system/public/assets/static/payment/*
-cd ~/paktor/web-gaigai/packages/system/public/assets/static/payment && cp -rf ~/learn/vue.js/vue-cli/webpack/dist/* .
-cd ~/paktor/web-gaigai && git add packages/system/public/assets/static/payment/*
+n use 5.12.0 build/build.js
+cd ~/paktor/github/web-gaigai/packages/system/public/assets/static/payment && rm -rf *
+cd ~/paktor/github/web-gaigai && git rm -r packages/system/public/assets/static/payment/*
+cd ~/paktor/github/web-gaigai/packages/system/public/assets/static/payment && cp -rf ~/learn/vue.js/vue-cli/webpack/dist/* .
+cd ~/paktor/github/web-gaigai && git add packages/system/public/assets/static/payment/*
 git commit -m "stripe update"
 git push
