fdd220c53899c59aab62986db7043b07f8f8ae59
yong <zengjunyong@gmail.com>
2018-06-27 10:58:22 +0800

    deploy.hk.sh, issues now

--- deploy.hk.sh
+++ deploy.hk.sh
@@ -1,8 +1,8 @@
-npm run build
-cd ~/paktor/web-heydouble/packages/system/public/assets/static/payment
+n use 5.12.0 build/build.js
+cd ~/paktor/github/web-heydouble/packages/system/public/assets/static/payment
 rm -rf *
 cp -rf ~/learn/vue.js/vue-cli/webpack/dist/* .
-cd ~/paktor/web-heydouble
+cd ~/paktor/github/web-heydouble
 git rm packages/system/public/assets/static/payment/*
 git add packages/system/public/assets/static/payment/*
 git commit -m "stripe HK update"
